a941c3a28a14b05e062dc828ce2cae2889b971fb
Timofei Poriadin <poryadinta@gmail.com>
2024-12-02 23:12:27 +0100

    add dark mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,6 +12,11 @@ export default {
       accent: "#909090",
       links: "#00BFA5",
       borders: "#E0E0E0",
+      ["dark-background"]: "#202020",
+      ["dark-primary"]: "#F0F0F0",
+      ["dark-accent"]: "#B0B0B0",
+      ["dark-links"]: "#00FFC8",
+      ["dark-borders"]: "#505050",
     },
     fontFamily: {
       inconsolata: ["Inconsolata", "Courier New", "Courier", "monospace"],
